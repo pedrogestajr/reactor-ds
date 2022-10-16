9ff8db67e9974e51673a06ebca9643fb12a6367b
Pedro Gesta <pedrogestajr@gmail.com>
2022-10-16 22:25:41 +1100

    Add Slot utility

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,9 +1,27 @@
-interface TextProps {
-  size?: 'sm' | 'md' | 'lg'
+import { ReactNode } from 'react';
+import { Slot } from "@radix-ui/react-slot";
+import { clsx } from 'clsx';
+
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: ReactNode;
+  asChild?: boolean;
 }
 
-export function Text({size = 'md'}: TextProps) {
-    return(
-      <span className="text-neutral-50 font-sans">Text</span>
-    )
+export function Text({size = 'md', children, asChild}: TextProps) {
+  const Comp = asChild ? Slot : 'span';
+  
+  return(
+    <Comp className={clsx(
+        "text-neutral-50 font-sans",
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        }
+      )}
+    >
+      {children}
+    </Comp>
+  )
 }
