583e7b11e1b078d388375ef47c50870bb6422047
Pedro Gesta <pedrogestajr@gmail.com>
2022-10-30 06:23:18 +1100

    Update Heading component
    - Add className to Heading interface and function parameters
    - Update text color

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -6,19 +6,21 @@ export interface HeadingProps {
   size?: 'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Heading({size = 'md', children, asChild}: HeadingProps) {
+export function Heading({size = 'md', children, asChild, className}: HeadingProps) {
   const Comp = asChild ? Slot : 'h2';
   
   return(
     <Comp className={clsx(
-        "text-neutral-50 font-sans",
+        "text-neutral-300 font-sans",
         {
           'text-3xl': size === 'sm',
           'text-4xl': size === 'md',
           'text-5xl': size === 'lg',
-        }
+        },
+        className
       )}
     >
       {children}
