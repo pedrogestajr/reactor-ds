6f4858559eac3013cf2997a6c6c7d0477158d9cc
Pedro Gesta <pedrogestajr@gmail.com>
2022-10-30 06:19:15 +1100

    Update Button component
    - Extend interface to ButtonHTMLAttributes
    - Add className and props to Button function

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,21 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { Slot } from "@radix-ui/react-slot";
 import { clsx } from 'clsx';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
   const Comp = asChild ? Slot : 'button';
   
   return(
     <Comp className={clsx(
         "py-3 px-3 bg-cyan-400 text-neutral-900 text-md font-semibold w-full rounded font-sans hover:bg-cyan-200 transition-colors",
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
